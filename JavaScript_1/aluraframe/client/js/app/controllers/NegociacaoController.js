69141468a4317bf5ba2f04673124be4460b6a10b
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-03 17:15:27 -0300

    Criando variável $ para querySelector mantendo a associação do this com document

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,9 +3,17 @@ class NegociacaoController {
   adiciona(event) {    
     event.preventDefault();
 
+<<<<<<< Updated upstream
     let inputData = document.querySelector('#data');
     let inputQuantidade = document.querySelector('#quantidade');
     let inputValor = document.querySelector('#valor');
+=======
+    let $ = document.querySelector.bind(documento);
+
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+>>>>>>> Stashed changes
 
     console.log(inputData.value);
     console.log(inputQuantidade.value);
