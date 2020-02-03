c257806e1ad280a38f1aa99e7ac651be16fbca8a
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-03 17:00:24 -0300

    Recuperando valores no controller

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,13 @@ class NegociacaoController {
 
   adiciona(event) {    
     event.preventDefault();
-    alert('Chamei ação no controller.')
 
-    }
+    let inputData = document.querySelector('#data');
+    let inputQuantidade = document.querySelector('#quantidade');
+    let inputValor = document.querySelector('#valor');
+
+    console.log(inputData.value);
+    console.log(inputQuantidade.value);
+    console.log(inputValor.value);
+  }
 }
