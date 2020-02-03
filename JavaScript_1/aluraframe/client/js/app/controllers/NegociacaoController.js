da9bb462e31e3858228ba24df3e1fb3409531ea7
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-03 17:13:27 -0300

    Revert "Criando variável $ para querySelector mantendo a associação do this com document"
    This reverts commit e8d25e13299844f22e54ea772578105be50ec750.

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,11 +3,9 @@ class NegociacaoController {
   adiciona(event) {    
     event.preventDefault();
 
-    let $ = document.querySelector();
-
-    let inputData = $('#data');
-    let inputQuantidade = $('#quantidade');
-    let inputValor = $('#valor');
+    let inputData = document.querySelector('#data');
+    let inputQuantidade = document.querySelector('#quantidade');
+    let inputValor = document.querySelector('#valor');
 
     console.log(inputData.value);
     console.log(inputQuantidade.value);
