10004dc70b69a979a8d64052ee167380d06c4560
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-03 17:57:10 -0300

    Criando a data com o valor recuperado da página e criando uma instância de negociacao

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,28 @@
 class NegociacaoController {
 
   constructor() {
+    
     let $ = document.querySelector.bind(document);
-    this.inputData = $('#data');
-    this.inputQuantidade = $('#quantidade');
-    this.inputValor = $('#valor');
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+
   }
 
-  adiciona(event) {    
+  adiciona(event) {
+    
     event.preventDefault();
 
-    console.log(this.inputData.value);
-    console.log(this.inputQuantidade.value);
-    console.log(this.inputValor.value);
+    let data = new Date(this._inputData.value.replace(/-/g, ','));
+    console.log(data);
+
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
   }
+
 }
