4bf770cd3d29e24ab9ba30af82a6a4d40ffdeb47
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-04 17:44:03 -0300

    Corrigindo o mês do input de data com função em map do array.

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,7 +13,16 @@ class NegociacaoController {
     
     event.preventDefault();
 
-    let data = new Date(...this._inputData.value.split('-'));
+    let data = new Date(...
+        this._inputData.value
+          .split('-')
+          .map(function(item, indice) {
+            if (indice == 1) {
+              return item - 1;
+            }
+            return item;
+          })
+    );
     console.log(data);    
   }
 
