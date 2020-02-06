b3bfd075a66509b11629a8a76fb86720940093c8
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-06 07:14:42 -0300

    Melhorando a expressão regular de validação de formato de data.

--- JavaScript_1/aluraframe/client/js/app/helpers/DateHelper.js
+++ JavaScript_1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -10,7 +10,7 @@ class DateHelper {
   }
   
   static validateDataFormat(text) {
-    if (!/\d{4}-\d{2}-\d{2}/.test(text))
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(text))
       throw new Error('Deve estar no formato aaaa-mm-dd');
   }
 
