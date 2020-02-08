7f165e9f277453d554f50f42a648842e15fb861a
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-08 05:11:10 -0300

    Adicionando MensagemView

--- JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript_1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,16 +7,25 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
     this._listaNegociacoes = new ListaNegociacoes();
-    this._negociacoesView = new NegociacoesView($('#tabelaNegociacoes'));
 
+    this._negociacoesView = new NegociacoesView($('#tabelaNegociacoes'));
     this._negociacoesView.update(this._listaNegociacoes);
+    
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView($('#mensagemAlerta'));
+    this._mensagemView.update(this._mensagem);
   }
 
   adiciona(event) {
     
     event.preventDefault();
+    
     this._listaNegociacoes.adiciona(this._criaNegociacao()); 
     this._negociacoesView.update(this._listaNegociacoes);
+
+    this._mensagem.texto = 'Negociação adicionada com sucesso';
+    this._mensagemView.update(this._mensagem);
+
     this._limpaFormulario();
   }
 
