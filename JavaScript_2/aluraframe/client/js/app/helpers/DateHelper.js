924920449fffb3321c839f08c7803c6e2f82434f
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-12 06:02:33 -0300

    Commit inicial do curso de javascript avançado 2

---
+++ JavaScript_2/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,24 @@
+class DateHelper {
+
+  constructor() {
+    throw new Error('DateHelper não pode ser instanciada.');
+  }
+  
+  static textToDate(text) {
+    DateHelper.validateDataFormat(text);
+    return new Date(...text.split('-').map((item, indice) => item - indice % 2));
+  }
+  
+  static validateDataFormat(text) {
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(text))
+      throw new Error('Deve estar no formato aaaa-mm-dd');
+  }
+
+  static dateToText(data) {
+    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+  }
+
+  static toDateInputValue(data) {
+    return data.toJSON().slice(0,10);
+  }
+}
---
+++ JavaScript_2/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,35 @@
+class Negociacao {
+
+  constructor(_data, _quantidade, _valor) {
+    
+    // constructor(data, quantidade, valor) {
+    // this._data = new Data(data.getTime());
+    // this._quantidade = quantidade;
+    // this._valor = valor;
+
+    // Object.assign(this, {
+    //   _data: new Date(data.getTime()),
+    //   _quantidade: quantidade,
+    //   _valor: valor});
+      
+    Object.assign(this, { _quantidade, _valor }); //Atribuindo os parâmetros às proriedades de classe com o mesmo nome.
+    this._data = new Date(_data.getTime());
+    Object.freeze(this);
+  }
+
+  get data() {
+    return new Date(this._data.getTime());
+  }
+
+  get quantidade() {
+    return this._quantidade;
+  }
+
+  get valor() {
+    return this._valor;
+  }
+
+  get volume() {
+    return this._quantidade * this._valor;
+  }
+}
