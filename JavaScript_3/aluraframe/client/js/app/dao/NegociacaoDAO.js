60ed0bea6514fd43a0423a74904b2a0b9254d8b2
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-20 08:04:26 -0300

    Adicionando método listarTodos na DAO.

--- JavaScript_3/aluraframe/client/js/app/dao/NegociacaoDAO.js
+++ JavaScript_3/aluraframe/client/js/app/dao/NegociacaoDAO.js
@@ -24,4 +24,51 @@ class NegociacaoDAO {
       }
     });
   }
+
+  listaTodos() {
+    return new Promise((resolve, reject) => {
+      let negociacoes = [];
+      let cursor = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .openCursor();
+
+        cursor.onsuccess = e => {
+          let atual = e.target.result;
+          if (atual) {
+            let dado = atual.value;
+            negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
+            atual.continue(); // Chama novamente o onsuccess.
+          } else {
+            resolve(negociacoes)
+          }
+        };
+        
+        cursor.onerror = e => {
+          console.log(e.target.error);
+          reject('Não foi possível recuperar as negociação do banco');
+        };
+    });
+  }
+
+
+  removeTodos() {
+
+    return new Promise((resolve, reject) => {
+      let request = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .remove(negociacao);
+
+      request.onsuccess = e => {
+        resolve('');
+      };
+
+      request.onerror = e => {
+        console.log(e.target.error);
+        reject('Não foi possível adicionar a negociação');
+      }
+    });
+  }
+
 }
