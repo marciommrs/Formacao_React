4944e744ab5201e2e600c7b61494fb74921d2663
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-23 10:06:01 -0300

    Alterando o método HttpService.get(url) para usar o fetch API.

--- JavaScript_3/aluraframe/client/js/app/services/HttpService.js
+++ JavaScript_3/aluraframe/client/js/app/services/HttpService.js
@@ -1,25 +1,14 @@
 class HttpService {
 
-  get(url) {
-    return new Promise((resolve, reject) => {
+  _handleErrors(res) {
+    if (!res.ok) throw new Error(res.statusText);
+    return res;
+  }
 
-      let xhr = new XMLHttpRequest();
-      
-      xhr.open('GET', url);
-      
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {    
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            console.log(xhr.responseText);
-            reject(xhr.responseText);
-  
-          }
-        }
-      }
-      xhr.send();
-    });
+  get(url) {
+    return fetch(url)
+      .then(res => this._handleErrors(res))
+      .then(res => res.json());
   }
 
   post(url, dado) {
