a16f11743f27132ee2fb980acfa6cef023362cbf
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-02-24 16:17:09 -0300

    Configurando o boot.js e o index.html para importação dinâmica pelo SystemJS

---
+++ JavaScript_3/aluraframe/client/js/app-es6/boot.js
@@ -0,0 +1,13 @@
+import {NegociacaoController} from './controllers/NegociacaoController';
+import {DateHelper} from './helpers/DateHelper';
+import {} from './polyfill/fetch';
+
+let negociacaoController = new NegociacaoController();
+
+//O this do adiciona dentro de adiciona e apaga deve ser mantido, por isso o bind.
+document.querySelector('.form').onsubmit = negociacaoController.adiciona.bind(negociacaoController);
+document.querySelector('[type=button]').onclick = negociacaoController.apaga.bind(negociacaoController);
+
+document.querySelector('#data').value = DateHelper.dateToText(new Date());
+//document.querySelector('#data').value = DateHelper.toDateInputValue(new Date());
+//Quando uso do tipo Date no input.
